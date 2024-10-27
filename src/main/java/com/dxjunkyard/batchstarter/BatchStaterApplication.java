e1aaabeae4b932964a7f3a264affa26e85a77c39
fooqoo <horseshoes.buzz.17@gmail.com>
2024-10-27 22:16:45 +0900

    build: create project

---
+++ src/main/java/com/dxjunkyard/batchstarter/BatchStaterApplication.java
@@ -0,0 +1,13 @@
+package com.dxjunkyard.batchstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchStaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchStaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dxjunkyard/batchstarter/BatchStaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxjunkyard.batchstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchStaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
